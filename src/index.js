966c5a28d0d3de37f6377f7b9fdc7ed0ae59d696
Norlock <jwillems25@hotmail.com>
2019-10-23 23:16:45 +0200

    Added some methods

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import _ from 'lodash';
+import { Node } from './node.js';
 
-function component() {
-    const element = document.createElement('div');
+document.addEventListener('DOMContentLoaded', (event) => {
+    //the event occurred
+    const element = document.getElementById('node-container');
+    const list = new Node("Blut Aus Nord");
+    list.append(new Node("Deathspell Omega"));
+    list.append(new Node("Drudkh"));
+    list.append(new Node("Urfaust"));
+    list.append(new Node("Sunn O)))"));
+    list.append(new Node("Insomnium"));
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    list.removeAt(3);
+    const node = list.getAt(4)
+    console.log('node: ' + node.name + ' previous: ' + node.previous.name);
+    element.innerText = list.print();
 
-    return element;
-}
+    //return element;
+});
 
-document.body.appendChild(component());
--- src/node.js
+++ src/node.js
@@ -1,5 +1,69 @@
 export class Node {  
-    constructor() {  
-        console.log('heeft node aangemaakt');
+
+    constructor(name) {  
+        this.name = name;
+        //this.id = id;
+
+        this.next = null;
+        this.previous = null;
+    }
+
+    append(node) {  
+        if(this.next === null) {  
+            node.previous = this;
+            this.next = node;
+        } else {  
+            this.next.append(node);
+        }
+        
+    }
+
+    print() {  
+        if (this.previous === null && this.next === null) {  
+            return "Head and tail is: " + this.name;
+        } else if (this.isHead()) {  
+            return "Head is: " + this.name + "\n" + this.next.print() ;
+        } else if (this.isTail()) {  
+            return "\nTail is: " +  this.name;
+        } else { 
+            return this.name + " " + this.next.print();
+        }
+    }
+
+    getAt(index) {  
+        if (index < 0) {  
+            alert('Index to low');
+        }
+
+        if (index === 0) {  
+            return this;
+        } else if (!this.isTail()) {  
+            return this.next.getAt(index - 1);
+        } else {  
+            alert('Index to high');
+        }
+    }
+
+    removeAt(index) {  
+        if (index < 0) {  
+            alert('Index to low');
+        }
+
+        if (index === 0) {  
+            this.previous.next = this.next;
+            this.next.previous = this.previous;
+        } else if (!this.isTail()) {  
+            this.next.removeAt(index - 1);
+        } else {  
+            alert('Index to high');
+        }
+    }
+
+    isHead() {  
+        return this.previous === null;
+    }
+
+    isTail() {  
+        return this.next === null;
     }
 }
